c00603cf163ae8cb2c981a6120bb4d0c518e1655
darthmaim <git@darthmaim.de>
2025-06-02 13:58:13 +0200

    Add `/v2/mailcarriers` types

---
+++ packages/types/data/mailcarrier.ts
@@ -0,0 +1,28 @@
+/**
+ * Mail Carrier (/v2/mailcarriers).
+ *
+ * @see https://wiki.guildwars2.com/wiki/API:2/mailcarriers
+ */
+export interface MailCarrier {
+  /** The mail carrier id */
+  id: number,
+
+  /** The mail carrier name */
+  name: string,
+
+  /** The mini icon */
+  icon: string,
+
+  /** The item id which unlocks this mail carrier. Can be resolved against `/v2/items`. */
+  unlock_items: number[],
+
+  /** The sort order that is used for displaying the mail carrier in-game */
+  order: number,
+
+  /** Flags for this mail carrier */
+  flags: MailCarrier.Flag[],
+}
+
+export namespace MailCarrier {
+  export type Flag = 'Default' | (string & {})
+}
